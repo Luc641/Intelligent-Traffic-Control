20de3e25f4f9972723ebc1a5045af34a12768dc3
Marvin Dinges <70886534+Marvin-Tate@users.noreply.github.com>
2021-11-02 11:09:55 +0100

    .

--- Implementation/src/main/java/TrafficLights/TrafficLightStates.java
+++ Implementation/src/main/java/TrafficLights/TrafficLightStates.java
@@ -1,4 +1,13 @@
 package TrafficLights;
 
 public enum TrafficLightStates {
+    RED,
+    YELLOW,
+    GREEN,
+    RED_YELLOW,
+    YELLOW_BLINKING,
+    GREEN_ARROW,
+    YELLOW_ARROW,
+    RED_ARROW
 }
+
