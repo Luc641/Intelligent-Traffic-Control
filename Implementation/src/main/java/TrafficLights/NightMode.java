edec50dc1dbed354b04026383bcf831f7adfbfc1
LucLehmkuhl <l.lehmkuhl@student.fontys.nl>
2021-11-11 11:36:46 +0100

    sequence method delegateable

--- Implementation/src/main/java/TrafficLights/NightMode.java
+++ Implementation/src/main/java/TrafficLights/NightMode.java
@@ -1,12 +1,14 @@
 package TrafficLights;
 
+import javax.sound.sampled.LineUnavailableException;
+
 public class NightMode implements TrafficLight{
 
     public TrafficLightStates state = TrafficLightStates.YELLOW_BLINKING;
 
     @Override
-    public void sequence() throws InterruptedException {
-
+    public void sequence() throws InterruptedException, LineUnavailableException {
+    TrafficLight.super.sequence();
     }
 
     @Override
--- Implementation/src/main/java/TrafficLights/TrafficLight.java
+++ Implementation/src/main/java/TrafficLights/TrafficLight.java
@@ -1,11 +1,21 @@
 package TrafficLights;
 
 import javax.sound.sampled.LineUnavailableException;
+import java.util.concurrent.TimeUnit;
 
 public interface TrafficLight {
 
+      default void sequence() throws InterruptedException, LineUnavailableException {
 
-     void sequence() throws InterruptedException, LineUnavailableException;
+          TrafficLightStates trafficLightStates = TrafficLightStates.GREEN;
+          System.out.println("" + trafficLightStates);
+          TimeUnit.SECONDS.sleep(45);
+          trafficLightStates = TrafficLightStates.YELLOW;
+          System.out.println("" + trafficLightStates);
+          TimeUnit.SECONDS.sleep(3);
+          trafficLightStates = TrafficLightStates.RED;
+          System.out.println("" + trafficLightStates);
+     }
 
      TrafficLightStates getTrafficLightStates();
 
