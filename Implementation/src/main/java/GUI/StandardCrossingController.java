a5b20db2f3af3d274ef147c5ea1a206bbe2a8472
LucLehmkuhl <l.lehmkuhl@student.fontys.nl>
2021-11-26 10:07:18 +0100

    gave the gui impl a try still trying to solve the problem of init the fxml file

--- Implementation/src/main/java/GUI/StandardCrossingController.java
+++ Implementation/src/main/java/GUI/StandardCrossingController.java
@@ -1,12 +1,25 @@
 package GUI;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.Objects;
+
+import Intersection.Intersection;
+import TrafficLights.TrafficLight;
+import TrafficLights.TrafficLightFactory;
+import TrafficLights.TrafficLightStates;
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
-public class StandardCrossingController {
+public class StandardCrossingController extends Application {
 
 
 
@@ -104,6 +117,63 @@ public class StandardCrossingController {
 
     @FXML
     void initialize() {
+        setColorWhite(trafficGreenSouth, trafficYellowSouth, trafficRedSouth, trafficGreenNorth, trafficYellowNorth, trafficRedNorth);
+        setColorWhite(trafficRedEast, trafficYellowEast, trafficRedEast, trafficRedWest, trafficYellowWest, trafficGreenWest);
+        changeColor();
+
     }
 
+
+    private void setColorWhite(Circle trafficGreenSouth, Circle trafficYellowSouth, Circle trafficRedSouth, Circle trafficGreenNorth, Circle trafficYellowNorth, Circle trafficRedNorth) {
+        trafficGreenSouth.setFill(Paint.valueOf("White"));
+        trafficYellowSouth.setFill(Paint.valueOf("White"));
+        trafficRedSouth.setFill(Paint.valueOf("White"));
+        trafficGreenNorth.setFill(Paint.valueOf("White"));
+        trafficYellowNorth.setFill(Paint.valueOf("White"));
+        trafficRedNorth.setFill(Paint.valueOf("White"));
+    }
+
+    public void changeColor (){
+        if (Objects.equals(TrafficLightStates.values(), "RED")){
+            trafficRedSouth.setFill(Paint.valueOf("Red"));
+            trafficRedNorth.setFill(Paint.valueOf("Red"));
+            trafficRedEast.setFill(Paint.valueOf("RED"));
+            trafficRedWest.setFill(Paint.valueOf("RED"));
+        } else if (Objects.equals(TrafficLightStates.values(), "YELLOW")){
+            trafficYellowEast.setFill(Paint.valueOf("Yellow"));
+            trafficYellowWest.setFill(Paint.valueOf("Yellow"));
+            trafficYellowSouth.setFill(Paint.valueOf("Yellow"));
+            trafficYellowNorth.setFill(Paint.valueOf("Yellow"));
+        }
+        trafficGreenSouth.setFill(Paint.valueOf("Green"));
+        trafficGreenNorth.setFill(Paint.valueOf("Green"));
+        trafficGreenEast.setFill(Paint.valueOf("Green"));
+        trafficGreenWest.setFill(Paint.valueOf("Green"));
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    private static Scene scene;
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        scene = new Scene(loadFXML("standardCrossing"));
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    protected static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StandardCrossingController.class.getResource(fxml + ".fxml"));
+        System.out.println(fxml);
+        // Set the controller depending on which view we load / which controller we need.
+        return fxmlLoader.load();
+    }
 }
