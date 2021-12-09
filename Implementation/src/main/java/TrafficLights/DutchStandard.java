5cd54e7d21bd6a819b95c17ebfe35c31d7c36eef
LucLehmkuhl <l.lehmkuhl@student.fontys.nl>
2021-12-09 10:10:36 +0100

    Added dutch traffic light states

--- Implementation/src/main/java/TrafficLights/DutchStandard.java
+++ Implementation/src/main/java/TrafficLights/DutchStandard.java
@@ -1,20 +1,16 @@
-//package TrafficLights;
-//
-//import java.util.concurrent.TimeUnit;
-//
-//public class DutchStandard extends AbstractTrafficLight {
-//
-//    private  TrafficLightStates state = TrafficLightStates.RED;
-//    int greenPhaseDuration = 5;
-//    private Thread thread;
-//    private final String name;
-//
-//    public DutchStandard(String name) {
-//        this.name = name;
-//    }
-//
-//    public DutchStandard(String name, TrafficLightStates state) {
-//        this.name = name;
-//        this.state = state;
-//    }
-//}
+package TrafficLights;
+
+import java.util.concurrent.TimeUnit;
+
+public class DutchStandard extends AbstractTrafficLight {
+
+    private TrafficLightStatesDutch state;
+
+    public DutchStandard(String name, State initialState) {
+        super(name,initialState);
+    }
+
+    public DutchStandard(String name) {
+        this(name, TrafficLightStatesDutch.GREEN);
+    }
+}
---
+++ Implementation/src/main/java/TrafficLights/TrafficLightStatesDutch.java
@@ -0,0 +1,53 @@
+package TrafficLights;
+
+public enum TrafficLightStatesDutch implements State {
+
+    RED {
+        @Override
+        public void showSignalOnce(Context context) {
+            context.changeState(RED_YELLOW);
+        }
+
+        @Override
+        public State inverse() {
+            return GREEN;
+        }
+    },
+
+    YELLOW {
+        @Override
+        public void showSignalOnce(Context context) {
+            context.changeState(RED);
+        }
+
+        @Override
+        public State inverse() {
+            return RED_YELLOW;
+        }
+    },
+
+    GREEN {
+        @Override
+        public void showSignalOnce(Context context) {
+            context.changeState(YELLOW);
+        }
+
+        @Override
+        public State inverse() {
+            return RED;
+        }
+
+    },
+    RED_YELLOW {
+        @Override
+        public void showSignalOnce(Context context) {
+            context.changeState(GREEN);
+        }
+
+        @Override
+        public State inverse() {
+            return YELLOW;
+        }
+    }
+}
+
